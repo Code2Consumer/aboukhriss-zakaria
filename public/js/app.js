33b713f5241d87f543fff012ed38be0f9fc1972f
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2019-12-05 21:25:39 +0100

    Adding new styles for skill design rework
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

--- public/js/app.js
+++ public/js/app.js
@@ -19373,8 +19373,8 @@ jQuery(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\aboukhriss-zakaria\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\aboukhriss-zakaria\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xamppp\htdocs\aboukhriss-zakaria\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xamppp\htdocs\aboukhriss-zakaria\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/templates/skill.blade.php
+++ resources/views/templates/skill.blade.php
@@ -1,5 +1,19 @@
 <div class="page-inner-container">
-    @foreach ($data as $tool)
-        {{ $tool->name }}
-    @endforeach
+    <ul id="hexGrid">
+        @foreach ($data as $tool)
+            <li class="hex">
+                <a class="hexIn" href="#">
+                    <img src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg" alt=""/>
+                    <h1>
+                        {{ $tool->name }}
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque commodi consequatur
+                        cupiditate dolore earum expedita, facere ipsum laborum nam placeat quam tempora, tenetur, ut
+                        voluptates. Ipsa quidem tempora tempore?
+                    </p>
+                </a>
+            </li>
+        @endforeach
+    </ul>
 </div>
